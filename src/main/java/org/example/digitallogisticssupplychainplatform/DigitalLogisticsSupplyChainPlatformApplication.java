77e4269009480712ad9dd1d305d7a3a287f309e9
yassine <yassineaghla23@gmail.com>
2025-10-27 16:57:43 +0100

    initialisation de projet

---
+++ src/main/java/org/example/digitallogisticssupplychainplatform/DigitalLogisticsSupplyChainPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.example.digitallogisticssupplychainplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalLogisticsSupplyChainPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalLogisticsSupplyChainPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/digitallogisticssupplychainplatform/DigitalLogisticsSupplyChainPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.digitallogisticssupplychainplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalLogisticsSupplyChainPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
